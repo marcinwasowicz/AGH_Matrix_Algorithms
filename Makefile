5f400ed244db99b89641cea5179192a0e29b44a4
marcinwasowicz <marcinwasowicz@LAPTOP-7TT223E8.localdomain>
2020-12-19 23:25:10 +0100

    Added task4 python version

---
+++ Makefile
@@ -0,0 +1,11 @@
+Task1:
+	gcc Task1.c -o bin/task1
+	./bin/task1
+Task2:
+	gcc Task2.c -o bin/task2
+	./bin/task2
+Task3:
+	gcc Task3.c -o bin/task3
+	./bin/task3
+Task4:
+	python3 Task4.py
---
+++ Task4.py
@@ -0,0 +1,66 @@
+EPSILON = 1e-14
+
+class Vertex:
+    def __init__(self):
+        self.as_row = {}
+        self.as_column = {}
+
+    def __repr__(self):
+        return str([self.as_row, self.as_column])
+
+class SparseMatrixGraph:
+    def __init__(self, dense_initializer):
+        self.graph = [Vertex() for _ in dense_initializer]
+
+        for row_idx, row in enumerate(dense_initializer):
+            for column_idx, value in enumerate(row):
+                if abs(value) > EPSILON:
+                    self[row_idx, column_idx] = value
+
+    def __getitem__(self, key):
+        row, column = key
+        return self.graph[row].as_row.get(column, 0.0)
+
+    def __setitem__(self, key, value):
+        row, column = key
+        self.graph[row].as_row[column] = value
+        self.graph[column].as_column[row] = value
+
+    def __repr__(self):
+        return str(self.graph)
+
+    def __iter__(self):
+        for row_idx in range(len(self.graph)):
+            for column_idx in range(len(self.graph)):
+                yield self[row_idx, column_idx]
+        
+    def lu_self_storage(self):
+        for idx in range(len(self.graph)):
+            for row_idx, _ in self.graph[idx].as_column.items():
+                if row_idx <= idx:
+                    continue
+                self[row_idx, idx] /= self[idx, idx]
+
+            for column_idx, _ in self.graph[idx].as_row.items():
+                if column_idx <= idx:
+                    continue
+
+                for row_idx, _ in self.graph[idx].as_column.items():
+                    if row_idx <= idx:
+                        continue
+
+                    value = -self[row_idx, idx] * self[idx, column_idx]
+                    if abs(self[row_idx, column_idx]) <= EPSILON:
+                        self[row_idx, column_idx] = value
+                    else:
+                        self[row_idx, column_idx] += value
+                
+
+dense = [
+    [1.0, 3.0, 0.0, 0.0, 0.0],
+    [0.0, 4.0, 5.0, 0.0, 0.0],
+    [0.0, 0.0, 6.0, 0.0, 0.0],
+    [2.0, 0.0, 0.0, 7.0, 0.0],
+    [0.0, 0.0, 0.0, 0.0, 8.0]
+]
+
