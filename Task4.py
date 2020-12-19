fc7049c62d7efdec23aaa931d122e638392e2e5d
marcinwasowicz <marcinwasowicz@LAPTOP-7TT223E8.localdomain>
2020-12-19 23:26:05 +0100

    removed example matrix from Task4.py

--- Task4.py
+++ Task4.py
@@ -54,13 +54,4 @@ class SparseMatrixGraph:
                         self[row_idx, column_idx] = value
                     else:
                         self[row_idx, column_idx] += value
-                
-
-dense = [
-    [1.0, 3.0, 0.0, 0.0, 0.0],
-    [0.0, 4.0, 5.0, 0.0, 0.0],
-    [0.0, 0.0, 6.0, 0.0, 0.0],
-    [2.0, 0.0, 0.0, 7.0, 0.0],
-    [0.0, 0.0, 0.0, 0.0, 8.0]
-]
 
